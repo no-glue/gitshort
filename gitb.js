26d99a22eea19422df0cbb769cd7b5f41ccdac8e
no-glue <nikolapav1985@gmail.com>
2014-11-28 16:47:55 +0100

    package

---
+++ gitb.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var sys = require("sys");
+var exec = require("child_process").exec;
+var puts = (function(sys) {
+    return function(error, stdout, stderr) {
+        sys.puts(stderr);
+    };
+})(sys);
+var myArgs = process.argv.slice(2);
+var len = myArgs.length;
+var branch = "nothing";
+//
+if(len === 1) {
+    branch = myArgs.shift();
+} else {
+    console.log("use as gitb abranch");
+    return;
+}
+exec("git checkout -b " + branch, puts);
